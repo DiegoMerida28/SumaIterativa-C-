819a5053eb77c695243f9d8c32d7f0f402d15c50
DiegoMerida28 <145516421+DiegoMerida28@users.noreply.github.com>
2023-10-28 20:31:48 -0600

    Agregar archivos de proyecto.

---
+++ SumaIterativa-C#/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingresa el primer número: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingresa el segundo número: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        int suma = 0;
+
+        for (int i = 0; i < num2; i++)
+        {
+            suma += num1;
+        }
+
+        Console.WriteLine($"La suma de {num1} y {num2} es: {suma}");
+    }
+}
